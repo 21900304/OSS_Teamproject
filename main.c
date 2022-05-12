b52fe58366e297ab20e49c99aad868b0667026aa
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 <21900304@handong.ac.kr>
2022-05-12 23:27:26 +0900

    수정 사항 수렴하여 수정하였습니다 main.c

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int main(void){
         Cafe d[20];
         int menu;
         int user;
-        int index=0;
+        int index=0; //음료의 리스크를 순서대로 보기 위함과 반복문을 구현할 때 범위의 기준을 잡기 위해 선언.
         int count=0;
-        
+
         user = selectUser();
         while(1)
         {
@@ -25,9 +25,26 @@ int main(void){
                   }
                   else if(menu == 2) listDessert(d,index);
                   else if(menu == 3) updataDessert(d);
-                  else if(menu == 4) deleteDessert(d);
-                  //else if(menu == 5) saveProduct(d);
-                  //else if(menu == 6) loadProduct(d); 
+                  else if(menu == 4)
+                  {
+                        int no = selectDateNo(d, index);
+          
+                        if(no==0)
+                        {
+                                printf("=> 취소됨!\n");
+                                continue;
+                        }
+                        int deleteok; //삭제를 원하는지 사용자에게 물어보는 변수
+                        printf("정말로 삭제하시겠습니까? (삭제 :1)");
+                        scanf("%d",&deleteok);
+                        if(deleteok==1)
+                        {
+                                if(deleteDessert(&d[no-1])) count--;
+                        }  
+
+                  }
+                  else if(menu == 5) SaveDate(d);
+                  else if(menu == 6) LoadDate(d); 
                   
                 }
                 if(user == 2)
