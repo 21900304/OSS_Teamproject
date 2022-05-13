02af47c05353da4bec42f1b8c69f452a8c3b3d9a
이한나 <tiger5391@naver.com>
2022-05-13 18:40:23 +0900

    변수 수정+while문, if문 수정+함수 호출 수정

--- main.c
+++ main.c
@@ -1,58 +1,47 @@
-#include "dessert.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "dessert.c"
  
 int main(void){
-        Cafe d[20];
-        int menu;
+	Cafe *menu_list[50]; //포인터 배열로 수정
+	int count = 0, menu; int index = 0; //음료의 리스크를 순서대로 보기 위함과 반복문을 구현할 때 범위의 기준을 잡기 위해 선언.
         int user;
-        int index=0; //음료의 리스크를 순서대로 보기 위함과 반복문을 구현할 때 범위의 기준을 잡기 위해 선언.
-        int count=0;
-
-        user = selectUser();
-        while(1)
-        {
-                
-                if(user==1)//판매자일 때
-                {
-
-                  menu = selectMenu();
-
-                  if(menu == 0) break;
-                  if(menu == 1)
-                  {
-                        count += createDessert(&d[index++]);
-                  }
-                  else if(menu == 2) listDessert(d,index);
-                  else if(menu == 3) updataDessert(d);
-                  else if(menu == 4)
-                  {
-                        int no = selectDateNo(d, index);
-          
-                        if(no==0)
-                        {
+	count = LoadDate(menu_list);
+    	index = count;
+	user = selectUser();
+	if(user==1){//판매자일 때
+		 while(1){
+			menu = selectMenu();
+                 	if(menu == 0) break;
+                  	if(menu == 1){
+			menu_list[index]=(Cafe *)malloc(sizeof(Cafe));
+                        count += createDessert(menu_list[index++]);
+                 	}
+                  	else if(menu == 2) listDessert(menu_list,index);
+                  	else if(menu == 3){
+			int num;//삭제 음료 번호 선택 변수
+                        listDessert(menu_list,index);
+                        printf("수정할 음료 번호? ");
+                        scanf("%d", &num);       
+			createDessert(menu_list[num-1]);
+			}
+                  	else if(menu == 4){
+                        int no = selectDateNo(menu_list, index);
+                        if(no==0){
                                 printf("=> 취소됨!\n");
                                 continue;
                         }
                         int deleteok; //삭제를 원하는지 사용자에게 물어보는 변수
                         printf("정말로 삭제하시겠습니까? (삭제 :1)");
                         scanf("%d",&deleteok);
-                        if(deleteok==1)
-                        {
-                                if(deleteDessert(&d[no-1])) count--;
-                        }  
-
-                  }
-                  else if(menu == 5) SaveDate(d);
-                  else if(menu == 6) LoadDate(d); 
-                  
+                        	if(deleteok==1){
+                                   if(deleteDessert(menu_list[no-1])) count--;
+                        	}  
+                  	}
+                 	else if(menu == 5) SaveDate(menu_list,index);
+                  }//while
+	}//user==1
+                if(user == 2){//구매자일 때
+			return 0;
                 }
-                if(user == 2)
-                {
-                    break;//구매자일 때
-                }
-                
-        }
         printf("종료됨");
         return 0;
 }
