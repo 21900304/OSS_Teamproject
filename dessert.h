0b3ec3310ede568cd652d58f490187d4d60e4ec7
이한나 <tiger5391@naver.com>
2022-05-13 18:18:23 +0900

    header파일 수정-변수 재정의

--- dessert.h
+++ dessert.h
@@ -8,17 +8,15 @@ typedef struct{
 
 int selectUser(); // 판매자와 구매자를 선택하는 함수
 int selectMenu(); // 판매자를 선택시 메뉴를 선택하는 함수
-int createDessert(); //판매자가 디저트 품목을 만드는 함수
-void listDessert(); // 어떤 디저트가 있는 확인할 수 있는 함수
-int readDessert();// 디저트품목을 읽어오는 함수
-
-int deleteDessert();//디저트 품목을 삭제하는 함수
-int selectDateNo(); // 사용자가 디저트 품목을 삭제하고 싶을 때 몇 번째 데이터를 삭제하는 확인하는 함수
-
-
-int updataDessert();//디저트 품목을 수정하는 함수
-void SaveDate();//데이터를 파일에 저장하는 함수
-int LoadDate();//데이터를 파일에서 로딩하는 함수
+int createDessert(Cafe *p); //판매자가 디저트 품목을 만드는 함수
+void listDessert(Cafe *p[],int index); // 어떤 디저트가 있는 확인할 수 있는 함수
+int readDessert(Cafe p, int i); // 디저트품목을 읽어오는 함수
+int deleteDessert(Cafe *p); //디저트 품목을 삭제하는 함수
+int selectDateNo(Cafe *s[], int index); // 사용자가 디저트 품목을 삭제하고 싶을 때 몇 번째 데이터를 삭제하는 확인하는 함수
+int updataDessert(Cafe *p); //디저트 품목을 수정하는 함수
+void SaveDate(Cafe *p[], int count); //데이터를 파일에 저장하는 함수
+int LoadDate(Cafe *p[]); //데이터를 파일에서 로딩하는 함수
+//남은 함수들
 void listConsumer();//소비자가 디저트 품목을 확인하고 구매를 하는 함수
 void decaffeine();// 소비자가 구매를 할 때 디카페인, 당도 등 자신의 취향을 결정하게 도와주는 함수
 int pointStemp();//소비자에게 스탬프를 찍어, 몇 회 이상 방문하였는지 확인할 수 있는 함수
